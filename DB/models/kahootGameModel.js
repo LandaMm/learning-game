5e375c6b20f55426e5bad11053f4f614f848ac66
LandaMm <qwer.009771@gmail.com>
2023-09-10 12:17:56 +0200

    feat: kahoot game model schema type

--- DB/models/kahootGameModel.js
+++ DB/models/kahootGameModel.js
@@ -1,9 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const KahootGameSchema = new mongoose.Schema({
-    id: Number,
+  pin: String,
+  hostId: String,
+  // TODO: check and provide correct type
+  gameLive: String,
+  gameData: Object,
 });
 
-const KahootGame = mongoose.model('kahootgames', KahootGameSchema);
+const KahootGame = mongoose.model("kahootgames", KahootGameSchema);
 
 module.exports = KahootGame;
