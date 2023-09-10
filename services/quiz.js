18896273ef2426e55258db163aa935069ed2ae38
LandaMm <qwer.009771@gmail.com>
2023-09-10 16:13:00 +0200

    feat: get all quizes svc

--- services/quiz.js
+++ services/quiz.js
@@ -8,6 +8,10 @@ class Quizes {
   async addQuiz(data) {
     return await this.model.create(data);
   }
+
+  async getAllQuizes() {
+    return await this.model.find({}).exec();
+  }
 }
 
 module.exports = Quizes;
