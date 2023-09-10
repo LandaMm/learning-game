eae74fd0c2a3a3c1dc31738a76ab6ab6b7860cdc
LandaMm <qwer.009771@gmail.com>
2023-09-10 16:01:23 +0200

    fix: remove unnecessary random id logic

--- socket/game/handlers/newQuiz.js
+++ socket/game/handlers/newQuiz.js
@@ -1,22 +1,9 @@
 const newQuizHandler = async (socket, data, utilities) => {
   try {
-    const allGames = await utilities.findAll();
-
-    if (!allGames) {
-      socket.emit("error", "An error occurred fetching all game data.");
-      return;
-    }
-
-    const numGames = allGames.length;
-
-    // Adjusting the game ID logic
-    const gameId = numGames === 0 ? 1 : allGames[numGames - 1].id + 1;
-    data.id = gameId;
-
     // Inserting the new game data
-    await utilities.insertNewGame(data);
+    const game = await utilities.insertNewGame(data);
 
-    socket.emit("startGameFromCreator", gameId);
+    socket.emit("startGameFromCreator", game._id);
   } catch (err) {
     console.error("Error in newQuiz function:", err);
     socket.emit("error", "An error occurred during the newQuiz operation.");
