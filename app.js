0e308e7f8039e09c1f1d225112111ff628d201f1
LandaMm <qwer.009771@gmail.com>
2023-09-16 15:05:38 +0200

    feat: app & socket.io loggers

--- app.js
+++ app.js
@@ -4,36 +4,10 @@ const express = require("express");
 const socketIO = require("socket.io");
 const connectDB = require("./DB/dbConnect");
 const initializeSocketHandlers = require("./socket/socketHandlers");
-const debug = require("debug");
 const cors = require("cors");
-const winston = require("winston");
+const { appLogger } = require("./logger");
+
 require("dotenv").config();
-
-const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.json(),
-  defaultMeta: { service: "user-service" },
-  transports: [
-    new winston.transports.File({ filename: "logs/error.log", level: "error" }),
-    new winston.transports.File({ filename: "logs/combined.log" }),
-  ],
-});
-
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
-if (process.env.NODE_ENV !== "production") {
-  logger.add(
-    new winston.transports.Console({
-      format: winston.format.simple(),
-    }),
-  );
-}
-
-const debugApp = debug("app");
-// TODO: find out if we really need this
-// const debugSocket = debug("socket.io");
 
 const publicPath = path.join(__dirname, "/public");
 
@@ -49,6 +23,6 @@ initializeSocketHandlers(io);
 
 connectDB().then(() => {
   server.listen(process.env.PORT, () => {
-    debugApp(`Server started on port ${process.env.PORT}`);
+    appLogger.info(`Server started on port ${process.env.PORT}`);
   });
 });
---
+++ logger.js
@@ -0,0 +1,50 @@
+const winston = require("winston");
+
+const customFormat = winston.format.printf(
+  ({ level, message, label, timestamp }) => {
+    return `${timestamp} [${label}] ${level}: ${message}`;
+  },
+);
+
+const loggerConfiguration = (label = "custom") => ({
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.label({ label: label }),
+    customFormat,
+  ),
+  expressFormat: true,
+  colorize: true,
+  transports: [
+    new winston.transports.File({ filename: "logs/error.log", level: "error" }),
+    new winston.transports.File({
+      filename: `logs/${label.replace(/\s+/gi, "_")}.log`,
+    }),
+  ],
+});
+
+const appLogger = winston.createLogger(loggerConfiguration("app"));
+const ioLogger = winston.createLogger(loggerConfiguration("socket.io"));
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== "production") {
+  appLogger.add(
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.timestamp(),
+        winston.format.label({ label: "app" }),
+        customFormat,
+      ),
+    }),
+  );
+}
+
+module.exports = {
+  loggerConfiguration,
+  appLogger,
+  ioLogger,
+};
