e3c0197b6380d21a2a7edd7a753be2198498d743
LandaMm <qwer.009771@gmail.com>
2023-09-16 14:42:00 +0200

    feat: winston logger

--- app.js
+++ app.js
@@ -6,7 +6,30 @@ const connectDB = require("./DB/dbConnect");
 const initializeSocketHandlers = require("./socket/socketHandlers");
 const debug = require("debug");
 const cors = require("cors");
+const winston = require("winston");
 require("dotenv").config();
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  defaultMeta: { service: "user-service" },
+  transports: [
+    new winston.transports.File({ filename: "logs/error.log", level: "error" }),
+    new winston.transports.File({ filename: "logs/combined.log" }),
+  ],
+});
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    }),
+  );
+}
 
 const debugApp = debug("app");
 // TODO: find out if we really need this
