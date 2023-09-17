c57e43f42cf1fe1dbac7c81f75468788c590de84
LandaMm <qwer.009771@gmail.com>
2023-09-17 13:42:52 +0200

    feat: todo complete

--- DB/models/game.js
+++ DB/models/game.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const GameSchema = new mongoose.Schema({
   pin: String,
   hostId: String,
-  // TODO: check and provide correct type
   gameLive: Boolean,
   gameData: Object,
 });
