901c69723d20456afbf0f4e2390879c8dde496cc
LandaMm <qwer.009771@gmail.com>
2023-09-17 11:57:06 +0200

    feat: admin router init

---
+++ admin/router.js
@@ -0,0 +1,22 @@
+const { Router } = require("express");
+const { adminLogger } = require("../logger");
+
+const adminRouter = Router();
+
+adminRouter.use((req, res, next) => {
+  adminLogger.info(
+    `request to "${req.path}" with data "${JSON.stringify(req.body)}"`,
+  );
+  next();
+});
+
+adminRouter.post("/login", (req, res) => {
+  const body = req.body;
+
+  // TODO: provide authorization
+  res.send({
+    statusCode: 200,
+    message: "Successfully logged in",
+    payload: body,
+  });
+});
