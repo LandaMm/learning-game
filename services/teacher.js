84772c3f098631f58119098275205e812ecb22fe
LandaMm <qwer.009771@gmail.com>
2023-09-24 20:11:56 +0200

    feat: generate tokens

--- services/teacher.js
+++ services/teacher.js
@@ -1,5 +1,8 @@
 const Teacher = require("../DB/models/teacher");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const TEACHER_TOKEN_SECRET = process.env.TEACHER_TOKEN_SECRET;
 
 class Teachers {
   constructor() {
@@ -16,6 +19,26 @@ class Teachers {
     const valid = await bcrypt.compare(password, user.password);
     if (!valid) throw new Error("Bad credentials.");
     return user.toJSON();
+  }
+
+  async getTokens(user) {
+    const payload = {
+      sub: user.email,
+    };
+    const accessToken = jwt.sign(payload, TEACHER_TOKEN_SECRET, {
+      algorithm: "HS256",
+      expiresIn: "1h",
+    });
+    const refreshToken = jwt.sign(payload, TEACHER_TOKEN_SECRET, {
+      algorithm: "HS256",
+      expiresIn: "1d",
+    });
+    const salt = await bcrypt.genSalt(10);
+    const hashedRefreshToken = await bcrypt.hash(refreshToken, salt);
+    return {
+      accessToken,
+      refreshToken: hashedRefreshToken,
+    };
   }
 }
 
