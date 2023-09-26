5f14bbf6782cd182de5c44e24ef548c113758aed
LandaMm <qwer.009771@gmail.com>
2023-09-26 20:34:37 +0200

    feat: teacher refresh route

--- teacher/router.js
+++ teacher/router.js
@@ -88,6 +88,49 @@ teacherRouter.post("/login", async (req, res) => {
   }
 });
 
+teacherRouter.post("/refresh", async (req, res) => {
+  const refreshToken = req.headers["authorization"];
+  if (!refreshToken) {
+    return res.status(400).json({
+      statusCode: 400,
+      message: "Refresh Token is required",
+    });
+  }
+
+  const decoded = teachers.decodeRefreshToken(refreshToken);
+
+  try {
+    const payload = jwt.verify(decoded, TEACHER_TOKEN_SECRET);
+
+    appLogger.info("verified refresh token", payload);
+
+    const email = payload.sub;
+    if (!email) {
+      return res.status(401).json({
+        statusCode: 401,
+        message: "Invalid refresh token.",
+      });
+    }
+
+    const user = await teachers.findByEmail(email);
+    if (!user) {
+      return res.status(401).json({
+        statusCode: 401,
+        message: "Invalid session",
+      });
+    }
+
+    const tokens = await teachers.getTokens(user);
+
+    res.status(200).json(tokens);
+  } catch (err) {
+    res.status(401).json({
+      statusCode: 401,
+      message: "Invalid refresh token",
+    });
+  }
+});
+
 teacherRouter.get("/quizes", teacherAuthGuard, async (req, res) => {
   appLogger.info("getting quizes for user", req.user);
   const userQuizes = await quizes.getTeacherQuizes(req.user._id);
