c015c4b7837d98f7bb39559f7532900fe412ce0d
LandaMm <qwer.009771@gmail.com>
2023-09-27 11:39:11 +0200

    feat: teacher model socketId prop

--- DB/models/teacher.js
+++ DB/models/teacher.js
@@ -4,6 +4,7 @@ const TeacherSchema = new mongoose.Schema(
   {
     fullName: String,
     email: String,
+    socketId: { type: String, default: null },
     password: { type: String, select: false },
     refreshToken: { type: String, default: null, select: false },
   },
