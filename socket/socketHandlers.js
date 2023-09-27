e7697c9c301e24173e76b4114ccf150c3bd64094
LandaMm <qwer.009771@gmail.com>
2023-09-27 11:39:47 +0200

    feat: get auth teacher from socket handshake

--- socket/socketHandlers.js
+++ socket/socketHandlers.js
@@ -2,10 +2,21 @@ const registerHostHandlers = require("./host");
 const registerGameHandlers = require("./game");
 const registerPlayerHandlers = require("./player");
 const { appLogger } = require("../logger");
+const Teachers = require("../services/teacher");
+
+const teachers = new Teachers();
 
 module.exports = (io) => {
-  io.on("connection", (socket) => {
+  io.on("connection", async (socket) => {
     appLogger.info("socket connected with handshake", socket.handshake.auth);
+
+    const auth = socket.handshake.auth;
+
+    if (auth?.token) {
+      const teacher = await teachers.findByAccessToken(auth.token);
+      teacher.socketId = socket.id;
+      await teacher.save();
+    }
 
     // host
     registerHostHandlers(socket, io);
