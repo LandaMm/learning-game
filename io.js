c6ad406f859aeb69217d35b74ffd8806c3cff20a
LandaMm <qwer.009771@gmail.com>
2023-09-27 11:38:54 +0200

    feat: io related things in separated file

---
+++ io.js
@@ -0,0 +1,43 @@
+const socketIO = require("socket.io");
+const { ioLogger } = require("./logger");
+
+const ioMiddleware = require("socketio-wildcard")();
+
+let IO = null;
+
+const registerIO = (httpServer, cb) => {
+  IO = socketIO(httpServer);
+
+  IO.use(ioMiddleware);
+
+  IO.on("connection", (socket) => {
+    ioLogger.info(`socket with "${socket.id}" id connected`);
+    socket.on("disconnect", () => {
+      ioLogger.info(`socket with "${socket.id}" id disconnected`);
+    });
+    socket.on("*", (packet) => {
+      const [eventName, eventData] = packet.data;
+      ioLogger.info(
+        `event "${eventName}" with ${
+          eventData ? JSON.stringify(eventData) : "no"
+        } data from "${socket.id}" socket`,
+      );
+    });
+    let _emit = socket.emit;
+
+    // decorate emit function
+    socket.emit = (...args) => {
+      _emit.apply(socket, args);
+      let { 0: eventName, 1: eventData } = args;
+      ioLogger.info(
+        `emit "${eventName}" event with ${
+          eventData ? JSON.stringify(eventData) : "no"
+        } data to "${socket.id}" socket`,
+      );
+    };
+  });
+
+  cb(IO);
+};
+
+module.exports = { IO, registerIO };
