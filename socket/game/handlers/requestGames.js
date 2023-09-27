0b30dbc9d38394c70a7145203db20ca09c0eee79
LandaMm <qwer.009771@gmail.com>
2023-09-27 12:27:06 +0200

    feat: get filter and log it

--- socket/game/handlers/requestGames.js
+++ socket/game/handlers/requestGames.js
@@ -1,7 +1,8 @@
 const { appLogger } = require("../../../logger");
 
-const requestGamesHandler = async (socket, quizes) => {
+const requestGamesHandler = async (socket, quizes, filter) => {
   try {
+    appLogger.info("get quizes with filter for socket", filter, socket.auth);
     const gamesList = await quizes.getAllQuizes();
     appLogger.info("gamesList", gamesList);
     socket.emit("gameNamesData", gamesList);
