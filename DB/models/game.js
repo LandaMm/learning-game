1b4054eefbdd716a68d787253aa3312914eb6ccc
LandaMm <qwer.009771@gmail.com>
2023-09-30 12:53:09 +0200

    feat: game timestamps

--- DB/models/game.js
+++ DB/models/game.js
@@ -3,15 +3,18 @@ const GameStats = require("./gameStats"); // Import the statistics model
 const Teacher = require("./teacher");
 
 // Define the Game schema
-const GameSchema = new mongoose.Schema({
-  pin: String,
-  hostId: String,
-  gameLive: Boolean,
-  gameFinished: { type: Boolean, default: false },
-  gameData: Object,
-  gameStats: { type: mongoose.Types.ObjectId, ref: GameStats }, // Reference to the statistics
-  createdBy: { type: mongoose.Types.ObjectId, ref: Teacher },
-});
+const GameSchema = new mongoose.Schema(
+  {
+    pin: String,
+    hostId: String,
+    gameLive: Boolean,
+    gameFinished: { type: Boolean, default: false },
+    gameData: Object,
+    gameStats: { type: mongoose.Types.ObjectId, ref: GameStats }, // Reference to the statistics
+    createdBy: { type: mongoose.Types.ObjectId, ref: Teacher },
+  },
+  { timestamps: true },
+);
 
 // Create the Game model
 const Game = mongoose.model("game", GameSchema);
