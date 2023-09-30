f38cabf68d1af98de5e63bc73c6905102d97c5dc
LandaMm <qwer.009771@gmail.com>
2023-09-30 12:53:18 +0200

    feat: player timestamps

--- DB/models/player.js
+++ DB/models/player.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 
-const PlayerSchema = new mongoose.Schema({
-  hostId: String,
-  playerId: String, // socket's id
-  name: String,
-  gameData: Object,
-});
+const PlayerSchema = new mongoose.Schema(
+  {
+    hostId: String,
+    playerId: String, // socket's id
+    name: String,
+    gameData: Object,
+  },
+  { timestamps: true },
+);
 
 const Player = mongoose.model("player", PlayerSchema);
 
