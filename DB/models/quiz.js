a3c454e97cc0af6482df3ddec9cb3be6c216fba2
LandaMm <qwer.009771@gmail.com>
2023-09-30 17:49:39 +0200

    feat: quiz created timestamp

--- DB/models/quiz.js
+++ DB/models/quiz.js
@@ -1,19 +1,22 @@
 const mongoose = require("mongoose");
 const Teacher = require("./teacher");
 
-const QuizSchema = new mongoose.Schema({
-  name: String,
-  /**
-   * Question type:
-   * {
-   *  question: String,
-   *  answers: Array,
-   *  correct: String
-   * }
-   */
-  questions: Array,
-  createdBy: { ref: Teacher, type: mongoose.Types.ObjectId, default: null },
-});
+const QuizSchema = new mongoose.Schema(
+  {
+    name: String,
+    /**
+     * Question type:
+     * {
+     *  question: String,
+     *  answers: Array,
+     *  correct: String
+     * }
+     */
+    questions: Array,
+    createdBy: { ref: Teacher, type: mongoose.Types.ObjectId, default: null },
+  },
+  { timestamps: true },
+);
 
 const Quiz = mongoose.model("quiz", QuizSchema);
 
